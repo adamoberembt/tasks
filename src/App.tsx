aec9e0a991499b1c6e89a19ddd3cfdbf6876d1ec
Adam oberembt <adamoberembt@adams-mbp.myfiosgateway.com>
2022-02-14 22:07:20 -0500

    Finished task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -10,6 +11,54 @@ function App(): JSX.Element {
             <p>
                 Hello World Edit <code>src/App.tsx</code> and save. This page
                 automatically reload.
+            </p>
+            <h1>New Header (header text)</h1>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg"
+                alt="Picture of a mountain"
+                width="400"
+                height="300"
+            />
+            <p>
+                Class List:
+                <ul>
+                    <li>First class</li>
+                    <li>second class</li>
+                    <li>third class</li>
+                </ul>
+            </p>
+            <p>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </p>
+            <p>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    border: "1px solid red",
+                                    padding: "4px",
+                                    backgroundColor: "red"
+                                }}
+                            >
+                                First column
+                            </div>
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    border: "1px solid red",
+                                    padding: "4px",
+                                    backgroundColor: "red"
+                                }}
+                            >
+                                Second column
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </p>
         </div>
     );
